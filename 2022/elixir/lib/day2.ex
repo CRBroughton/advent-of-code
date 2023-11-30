41b29ce04e84109dd664709e7b30b709f5f1309d
CRBroughton <crbroughton@posteo.uk>
2023-11-30 08:43:08 +0000

    feat: :white_check_mark: remove unrequired code, optimise day II

--- 2022/elixir/lib/day2.ex
+++ 2022/elixir/lib/day2.ex
@@ -5,71 +5,37 @@ defmodule Day2 do
   end
 
   def getRoundResult(rounds) do
-    fightResults = rounds
+    rounds
     |> sanitizeRounds()
     |> Enum.map(&fight(&1))
     |> Enum.sum()
-
-    shapeResults = rounds
-    |> sanitizeRounds()
-    |> Enum.map(&getPlayerShape(&1))
-    |> Enum.sum()
-
-    Enum.sum([fightResults, shapeResults])
   end
 
   def getFixedMatchResults(rounds) do
-    fightResults = rounds
+    rounds
     |> sanitizeRounds()
     |> Enum.map(&fixTheMatch(&1))
     |> IO.inspect()
     |> Enum.map(&fight(&1))
     |> Enum.sum()
-
-    shapeResults = rounds
-    |> sanitizeRounds()
-    |> Enum.map(&fixTheMatch(&1))
-    |> Enum.map(&getPlayerShape(&1))
-    |> Enum.sum()
-
-    Enum.sum([fightResults, shapeResults])
   end
 
   def fight(round) do
     case round do
-      # OPP - A = ROCK, B = PAPER = C SICSSORS
-      # PLAYER - X = ROCK, Y = PAPER, Z = SICSSORS
-      # CONDITIONS - LOSE = 0, DRAW = 1, WIN = 6
-      "A X" -> 3
-      "A Y" -> 6
-      "A Z" -> 0
-      "B X" -> 0
-      "B Y" -> 3
-      "B Z" -> 6
-      "C X" -> 6
-      "C Y" -> 0
-      "C Z" -> 3
-      _ -> 0
-    end
-  end
-
-  def getPlayerShape(round) do
-    case round do
-      "A Y" -> 2
-      "A X" -> 1
-      "A Z" -> 3
-      "B Y" -> 2
-      "B X" -> 1
-      "B Z" -> 3
-      "C Y" -> 2
-      "C X" -> 1
-      "C Z" -> 3
+      "A Y" -> 6 + 2
+      "A X" -> 3 + 1
+      "A Z" -> 0 + 3
+      "B X" -> 0 + 1
+      "B Y" -> 3 + 2
+      "B Z" -> 6 + 3
+      "C X" -> 6 + 1
+      "C Y" -> 0 + 2
+      "C Z" -> 3 + 3
       _ -> 0
     end
   end
 
   def fixTheMatch(round) do
-    # X = LOSE, Y = DRAW, Z = WIN
     case round do
       "A Y" -> "A X"
       "A X" -> "A Z"
