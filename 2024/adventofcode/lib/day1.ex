bc8de94740c8781933dec7e35ff3df5ea6840fd7
CRBroughton <crbroughton@posteo.uk>
2024-12-01 17:00:09 +0000

    feat: :tada: solve second half

--- 2024/adventofcode/lib/day1.ex
+++ 2024/adventofcode/lib/day1.ex
@@ -1,6 +1,18 @@
 defmodule Day1 do
   @type integer_tuple :: {integer(), integer()}
 
+
+  def createLists(input) do
+    lines = String.split(input, "\n", trim: true)
+
+    {lefts, rights} = Enum.map(lines, fn line ->
+      [left, right] = String.split(line)
+      {String.to_integer(left), String.to_integer(right)}
+    end)
+    |> Enum.unzip()
+    {lefts, rights}
+  end
+
   @spec createTupleList(String.t()) :: list({integer(), integer()})
   def createTupleList(input) do
     lines = String.split(input, "\n", trim: true)
@@ -19,6 +31,16 @@ defmodule Day1 do
       abs(left - right)
     end)
     Enum.sum(differences)
+  end
+
+  def sumUpSimilarities(input) do
+    {lefts, rights} = createLists(input)
+    frequencies = Enum.frequencies(rights)
+
+    Enum.map(lefts, fn number ->
+      number * Map.get(frequencies, number, 0)
+    end)
+    |> Enum.sum()
   end
 
   @spec result(binary()) :: integer()
